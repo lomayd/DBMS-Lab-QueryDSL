6a628e8f20924a49ce1e04c7c10190c0dc4cba82
lomayd <jinho10167@gmail.com>
2023-05-21 23:45:07 +0900

    feat: Project Initialize

---
+++ src/main/java/lomayd/DBMSLabQueryDSL/api/DbmsLabQueryDslApplication.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabQueryDSL.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsLabQueryDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsLabQueryDslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/DBMSLabQueryDSL/DbmsLabQueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabQueryDSL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsLabQueryDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
